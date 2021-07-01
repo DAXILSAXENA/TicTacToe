023de9c7e32252f9caa82b0b1cadc3152426d24d
DAXILSAXENA <daxildaxil@gmail.com>
2021-07-01 11:31:19 +0530

    "Added Feature to create new board"

--- src/com/bridgelabz/TicTacToe/TicTacToeGame.java
+++ src/com/bridgelabz/TicTacToe/TicTacToeGame.java
@@ -1,7 +1,18 @@
 package com.bridgelabz.TicTacToe;
 
 public class TicTacToeGame {
+
+	/* UC 1 */
+	private static char[] createBoard() {
+		char[] board = new char[10];
+		for (int i = 0; i < board.length; i++) {
+			board[i] = ' ';
+		}
+		return board;
+	}
+
 	public static void main(String[] args) {
 		System.out.println("Welcome to TicTacToe Game");
+		char[] board = createBoard();
 	}
 }
